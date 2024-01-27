a97b768df0e8fca6cb01b7a1312af6de72568fa2
Gabriel-ISS <gabriel69iss@gmail.com>
2024-01-27 11:13:49 -0300

    excluir archivos .env y cambiar variable de entorno para compatibilidad con vercel

--- app/_lib/aws-s3.ts
+++ app/_lib/aws-s3.ts
@@ -6,7 +6,7 @@ function getClient() {
     return new S3({
       region,
       credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY || '',
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
         secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || ''
       }
     })
