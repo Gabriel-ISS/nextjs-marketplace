4e8320b2ac8353672fcddf76d8dfd88cb1345b13
Gabriel-ISS <gabriel69iss@gmail.com>
2024-01-28 11:36:32 -0300

    corrección de conexión

--- app/_lib/db.ts
+++ app/_lib/db.ts
@@ -3,11 +3,15 @@ import 'server-only';
 import { ServerSideError } from '@/_lib/server-utils';
 import mongoose from 'mongoose';
 
-const { MONGO_URL } = process.env;
+const { MONGO_URL, MONGO_DB_NAME } = process.env;
 
 if (!MONGO_URL) throw new ServerSideError("MONGO_URL is not defined.", { send: true });
+if (!MONGO_DB_NAME) throw new ServerSideError("MONGO_DB_NAME is not defined.", { send: true });
 
-let cached = (global as any).mongoose;
+type Connection = {
+  conn: null | typeof mongoose
+}
+let cached = (global as any).mongoose as Connection;
 
 if (!cached) {
   cached = (global as any).mongoose = { conn: null };
@@ -16,7 +20,7 @@ if (!cached) {
 export const connectDB = async () => {
   if (cached.conn) return cached.conn;
 
-  cached.conn = await mongoose.connect(MONGO_URL);
+  cached.conn = await mongoose.connect(MONGO_URL, { dbName: MONGO_DB_NAME });
 
   return cached.conn;
 }
