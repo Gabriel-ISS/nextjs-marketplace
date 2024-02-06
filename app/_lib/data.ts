1ea762ad3ce047e9590b1b1808f0da43c38cf8a9
Gabriel-ISS <gabriel69iss@gmail.com>
2024-02-06 17:15:31 -0300

    reparación de error de axios en producción

--- app/_lib/data.ts
+++ app/_lib/data.ts
@@ -1,19 +1,36 @@
-import axios from 'axios'
+/* import axios from 'axios'
 
 const market = axios.create({
   baseURL: `${process.env.NEXT_PUBLIC_HOST}/api`
-})
+}) */
+
+type GetOptions = {
+  params?: any
+}
+
+function get<T>(path: string, options?: GetOptions): Promise<T> {
+  let query = ''
+  if (options?.params) {
+    query = '?' + new URLSearchParams(options.params).toString()
+  }
+  return fetch(`${process.env.NEXT_PUBLIC_HOST}/api${path}${query}`, {
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  })
+  .then(res => res.json())
+}
 
 export type GetGroupsParams = { NC?: boolean }
 export type GetGroupsReturn<P extends GetGroupsParams = GetGroupsParams> = ActionRes<P['NC'] extends true ? string[] : Group[]>
 export async function getProductGroups<P extends GetGroupsParams>(params?: P) {
-  return (await market.get<GetGroupsReturn<P>>('/groups', { params })).data
+  return (await get<GetGroupsReturn<P>>('/groups', { params }))
 }
 
 export type GetCategoriesParams = { includeImages?: boolean }
 export type GetCategoriesReturn<P extends GetCategoriesParams = GetCategoriesParams> = ActionRes<P['includeImages'] extends true ? CategoryWithImage[] : string[]>
 export async function getCategories<P extends GetCategoriesParams>(params?: P) {
-  return (await market.get<GetCategoriesReturn<P>>('/categories', { params })).data
+  return (await get<GetCategoriesReturn<P>>('/categories', { params }))
 }
 
 export type CategoryWithImage = { name: string, image: string }
@@ -21,18 +38,18 @@ export type GetFiltersParams = { category: string }
 export type GetFiltersReturn = ActionRes<FilterForFilters>
 export async function getCategoryFilters(category: string) {
   const params: GetFiltersParams = { category }
-  return (await market.get<GetFiltersReturn>('/filters', { params })).data
+  return (await get<GetFiltersReturn>('/filters', { params }))
 }
 
 type ProductsAndPages = { products: Product[], totalPages: number }
 export type GetProductsReturn = ActionRes<ProductsAndPages>
 export async function getProducts(queryString: string) {
-  return (await market.get<GetProductsReturn>(`/products?${queryString}`)).data
+  return (await get<GetProductsReturn>(`/products?${queryString}`))
 }
 
 export type GetProductParams = { id?: string }
 export type GetProductReturn = ActionRes<Product>
 export async function getProduct(id?: string) {
   const params: GetProductParams = { id }
-  return (await market.get<GetProductReturn>('/product', { params })).data
+  return (await get<GetProductReturn>('/product', { params }))
 }
