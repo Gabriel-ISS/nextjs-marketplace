6b042bb2131fc091f047f3612d17dc8f5a031f90
Gabriel-ISS <gabriel69iss@gmail.com>
2024-02-15 19:52:18 -0300

    ...

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
       <body className={openSans.className}>
         <Modal />
         <header style={{ zIndex: 1 }}>
-          <Suspense>
+          <Suspense fallback={<></>}>
             <Navigation />
           </Suspense>
         </header>
