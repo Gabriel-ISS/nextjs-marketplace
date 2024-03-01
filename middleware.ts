2edb6125f8d036b48aab3106d720ba001916c39e
Gabriel-ISS <gabriel69iss@gmail.com>
2024-03-01 11:39:57 -0300

    ...

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,5 @@
 import { ADMIN_ROLES, NOT_AUTHENTICATED_ERROR, UNAUTHORIZED_USER_ERROR } from '@/constants'
-import { Session } from 'next-auth'
+import { decode } from 'next-auth/jwt'
 import type { NextRequest } from 'next/server'
 import { NextResponse } from 'next/server'
 
@@ -12,26 +12,13 @@ export async function middleware(req: NextRequest) {
 
   if (CHECK_IF_ADMIN_ROUTES.includes(requestPath)) {
     let sessionToken = req.cookies.get('next-auth.session-token')?.value
-    const headers: HeadersInit = {
-      "Content-Type": "application/json"
-    }
-    if (sessionToken) {
-      headers["Cookie"] = `next-auth.session-token=${sessionToken};path=/;expires=Session`
-    }
-    const endpoint = process.env.NEXT_PUBLIC_HOST + '/api/categories'
-    const session: Session | null = await fetch(endpoint,
-      { method: 'GET', headers, cache: 'no-store' }
-    ).then(res => {
-      if (!res.ok) {
-        throw new Error(`HTTP error! status: ${res.status}\nAt route ${endpoint}`);
-      }
-      return res.json()
+    const userDecodedToken = await decode({
+      token: sessionToken,
+      secret: process.env.NEXTAUTH_SECRET as string,
     })
-    return NextResponse.json(session)
 
-    /* if (!session) return new NextResponse(NOT_AUTHENTICATED_ERROR, { status: 401 })
-    if (!ADMIN_ROLES.includes(session.user.role as string)) return new NextResponse(UNAUTHORIZED_USER_ERROR, { status: 403 })
-   */
+    if (!userDecodedToken) return new NextResponse(NOT_AUTHENTICATED_ERROR, { status: 401 })
+    if (!ADMIN_ROLES.includes(userDecodedToken.role as string)) return new NextResponse(UNAUTHORIZED_USER_ERROR, { status: 403 })
   }
 
   return NextResponse.next()
